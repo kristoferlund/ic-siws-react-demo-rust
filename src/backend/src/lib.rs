ecfc9a889886aeb0d42704c7643d3da27df59af9
Kristofer <kristofer@fmckl.se>
2024-04-02 10:53:48 +0200

    backend

---
+++ src/backend/src/lib.rs
@@ -0,0 +1,24 @@
+mod service;
+mod user_profile;
+
+use candid::Principal;
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap};
+use std::cell::RefCell;
+use user_profile::UserProfile;
+
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+type GetAddressResponse = Result<String, String>;
+
+thread_local! {
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    static USER_PROFILES: RefCell<StableBTreeMap<String, UserProfile, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))),
+        )
+    );
+
+    static SIWS_PROVIDER_CANISTER: RefCell<Option<Principal>>  = RefCell::new(None);
+}
---
+++ src/backend/src/service/init_upgrade.rs
@@ -0,0 +1,23 @@
+use candid::Principal;
+use ic_cdk::{init, post_upgrade};
+
+use crate::SIWS_PROVIDER_CANISTER;
+
+/// When initializing the canister, save a reference to the siwe provider canister.
+#[init]
+async fn init(siws_provider_canister: String) {
+    save_siws_provider_canister(siws_provider_canister);
+}
+
+/// When upgrading the canister, save a reference to the siwe provider canister.
+#[post_upgrade]
+fn upgrade(siws_provider_canister: String) {
+    save_siws_provider_canister(siws_provider_canister);
+}
+
+fn save_siws_provider_canister(siws_provider_canister: String) {
+    SIWS_PROVIDER_CANISTER.with(|canister| {
+        *canister.borrow_mut() =
+            Some(Principal::from_text(siws_provider_canister).expect("Invalid principal"));
+    });
+}
